702de3b8b054446f446c9c1c5ed28db250219d90
Suzi Nogueira <asuzinogueira@gmail.com>
2021-10-22 00:12:49 -0300

    finalizando a stage 4

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'suzinogueira',
+  youtube: '',
+  facebook: '',
+  instagram: 'suzi_nogueira',
+  twitter: ''
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userAvatar.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
